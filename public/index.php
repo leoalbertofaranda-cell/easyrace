ddf81cf5e364f33ed973b5819444c0b83fa57690
it.FarandaMedia <info@faranda.media>
2026-01-09 16:46:11 +0100

    Fix parse error index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
-cat > public/index.php <<'PHP'
 <?php
 require_once __DIR__ . '/../app/includes/bootstrap.php';
 
 echo "<h1>" . h(($config['app_name'] ?? 'EasyRace')) . " avviato</h1>";
 echo "<p>Ambiente: " . h(($config['env'] ?? 'local')) . "</p>";
-PHP
